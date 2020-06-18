a40ab230082abc4963fd0a8ce1b39de1fbff6733
JIEUN <JIEUN@DESKTOP-1JFASC3>
2020-06-18 21:56:51 +0900

    Second Day Naver Login - callback.jsp / LoginController / loginPage.jsp

--- src/main/java/com/project/core/controller/LoginController.java
+++ src/main/java/com/project/core/controller/LoginController.java
@@ -1,5 +1,12 @@
 package com.project.core.controller;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +25,42 @@ public class LoginController {
 		
 		return "junho/join.tiles";
 	}
+	
+	@RequestMapping(value = "/callback")
+	public String callback(Model model) {
+		
+		return "junho/callback";
+	}
+	
+	@RequestMapping(value = "/personalInfo")
+	public void personalInfo(HttpServletRequest request) throws Exception {
+	        String token = "AAAAOD-qZze84nq9XI7lNLBKRv04-i120f6XlJ7gz-Kecu3OYyl_M8TQzboBUSGiyJJ4hHBPfm0MFSc_fsoSy4mS4bI";// 네이버 로그인 접근 토큰; 여기에 복사한 토큰값을 넣어줍니다.
+	        String header = "Bearer " + token; // Bearer 다음에 공백 추가
+	        try {
+	            String apiURL = "https://openapi.naver.com/v1/nid/me";
+	            URL url = new URL(apiURL);
+	            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+	            con.setRequestMethod("GET");
+	            con.setRequestProperty("Authorization", header);
+	            int responseCode = con.getResponseCode();
+	            BufferedReader br;
+	            if(responseCode==200) { // 정상 호출
+	                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+	            } else {  // 에러 발생
+	                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+	            }
+	            String inputLine;
+	            StringBuffer response = new StringBuffer();
+	            while ((inputLine = br.readLine()) != null) {
+	                response.append(inputLine);
+	            }
+	            br.close();
+	            System.out.println(response.toString());
+	        } catch (Exception e) {
+	            System.out.println(e);
+	        }
+	}
+
 	
 	@RequestMapping("/access_denied_page")
 	public String accessDeniedPage() throws Exception {
